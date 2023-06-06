fdaaacc95bb7b02025c2f0e4ffa4020d70054ecb
Girleide de Araujo <115583142+girleidearaujoo@users.noreply.github.com>
2023-06-06 10:31:34 -0300

    Update classes.js

--- [POO - Manhã][Aula - Classes][Cat_Marley e Gi]/classes.js
+++ [POO - Manhã][Aula - Classes][Cat_Marley e Gi]/classes.js
@@ -16,5 +16,29 @@ class Tv{
         this.tamanho = tamanho;
         this.foto = foto;
     }
+        exibirNoHTML(){
+        //Deixando vazio o conteúdo da tag com id 'produtos'
+        document.getElementById('produtos').innerHTML = '';
+
+        for (let i = 0; i < this.listaDeProdutos.length; i++) {
+
+            document.getElementById('produtos').innerHTML += (`
+            <div class='card'>
+            
+                <img class='foto' src='img/${this.listaDeProdutos[i].foto}' />
+                <div class='info'>
+                    <h3 class='modelo'>${this.listaDeProdutos[i].modelo}</h3>
+                    <p class='marca'>${this.listaDeProdutos[i].marca}</p>
+                    
+                    <span class='outros'>${this.listaDeProdutos[i].tamanho}</span>
+                    <span class='outros'> polegadas</span>
+                </div>
+                <button onclick="">Remover</button>
+            </div>
+        `);
+            
+        }
+    }
+}
 }
 // oi david :)
