2b419848232e08055d15377b98ab045a0193b38e
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-08-29 09:24:41 -0300

    Create classes.js

---
+++ [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/classes.js
@@ -0,0 +1,9 @@
+class Aluno{
+    constructor(nome, turma, matricula, idade, foto){
+        this.nome = nome,
+        this.turma = turma,
+        this.matricula = matricula,
+        this.idade = idade,
+        this.foto = foto
+    }
+}
