3650b64fd015bc3b953941339520d42e8d568bc9
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-09-04 10:42:37 -0300

    Update objetos.js

--- [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/objetos.js
+++ [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/objetos.js
@@ -1,9 +1,26 @@
 let a2 = new Aluno("Darline", "2 ano", "8947152AL", 17, 'https://drive.google.com/file/d/1mGY2Y1oyIks5f7OvgCtd5lEysC5oe1uX')
-let a3 = new Aluno("Girleide", "2 ano", "3326453AL", 16, 'https://drive.google.com/file/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r')
+let a3 = new Aluno("Jaciane", "2 ano", "3326453AL", 16, 'https://drive.google.com/file/d/1d5oA9rmApqBH6Mt3n75Dto7bETmuus6x')
+let a4 = new Aluno("Girleide", "2 ano", "098635AL", 17, 'https://drive.google.com/file/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r')
+let a5 = new Aluno("Thiago", "2 ano", "92863AL", 16, 'https://drive.google.com/file/d/1yK1BWGSRxuHt4gP6mmYNOIRHs4plJ8gR')
+let a6 = new Aluno("Kemylly", "2 ano", "28364AL", 17, 'https://drive.google.com/file/d/1uWL4Z7JRW478UM_pObd4dGNp--USG5or')
+let a7 = new Aluno("Ellen", "2 ano", "0292827AL", 16, 'https://drive.google.com/file/d/1xkGio1rSh25XTFnKAHcwXXsFliwU22HQ')
+let a8 = new Aluno("Thayslane", "2 ano", "36436AL", 17, 'https://drive.google.com/file/d/1kQcfoPxzC3k0Jje8t6Qwo8we7JQhsdvR')
+let a9 = new Aluno("Catarina", "2 ano", "376253AL", 16, 'https://drive.google.com/file/d/1tCJ1hVCp7445_lj-Dj4LZ0yOaTAfxwA2')
+let a10 = new Aluno("Rebeca", "2 ano", "297373AL", 16, 'https://drive.google.com/file/d/1vw8sfjAXTlZpfwTJNJSXJQd1sOgKiVb0')
+let a11 = new Aluno("Helysson", "2 ano", "3736552AL", 17, 'https://drive.google.com/file/d/1IVyT8vEVgMC7UoKOcgqo3Nol9tBSmSLs')
 
-let listaDeAlunos = []
-listaDeAlunos.push(a2);
-listaDeAlunos.push(a3);
+
+let listaDeAlunos = [a2, a3, a4, a5, a6, a7, a8, a9 ,a10, a11]
+
+listaDeAlunos.sort((a, b) => {
+    if (a.nome[0] > b.nome[0]) {
+        return 1
+    } else if (a.nome[0] < b.nome[0]){
+        return -1
+    } else {
+        return 0
+    }
+})
 
 for(let i = 0; i < listaDeAlunos.length; i++){
     document.write(`
