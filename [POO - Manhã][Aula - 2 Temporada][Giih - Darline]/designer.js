527bf6cc990e723d7db0497984b3c8ac2750544f
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-09-11 10:43:09 -0300

    Create designer.js

---
+++ [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/designer.js
@@ -0,0 +1,28 @@
+for(let i = 0; i < listaDeAlunos.length; i++){
+    document.write(`
+    <div class='card'>
+
+        <div class='cabecalho'>
+            <div>
+                <p class='turma'>${listaDeAlunos[i].turma}</p>
+                <h3 class='nome'>${listaDeAlunos[i].getNome()}</h3>
+            </div>
+            <img class='logo' src='img/logoifal.png'/>
+        </div>
+    
+        <div class='conteudo'>
+            <img class='foto' src='${listaDeAlunos[i].foto.replace('https://drive.google.com/file', 'https://lh3.googleusercontent.com')}'>
+            <div class='info'>
+                <div>
+                    <p class='label'>Idade</p>
+                    <p class='valor'>${listaDeAlunos[i].getIdade()}</p>
+                </div>
+                <div>
+                    <p class='label'>Matrícula</p>
+                    <p class='valor'>${listaDeAlunos[i].matricula}</p>
+                </div>
+            </div>
+        </div>
+
+    </div>`)
+}
