995a93204b88a176cee1ba476923f10bec4f0e78
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-09-11 09:02:56 -0300

    Update objetos.js

--- [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/objetos.js
+++ [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/objetos.js
@@ -1,21 +1,21 @@
-let a2 = new Aluno("Darline", "2 ano", "8947152AL", 17, 'https://drive.google.com/file/d/1mGY2Y1oyIks5f7OvgCtd5lEysC5oe1uX')
-let a3 = new Aluno("Jaciane", "2 ano", "3326453AL", 16, 'https://drive.google.com/file/d/1d5oA9rmApqBH6Mt3n75Dto7bETmuus6x')
-let a4 = new Aluno("Girleide", "2 ano", "098635AL", 17, 'https://drive.google.com/file/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r')
-let a5 = new Aluno("Thiago", "2 ano", "92863AL", 16, 'https://drive.google.com/file/d/1yK1BWGSRxuHt4gP6mmYNOIRHs4plJ8gR')
-let a6 = new Aluno("Kemylly", "2 ano", "28364AL", 17, 'https://drive.google.com/file/d/1uWL4Z7JRW478UM_pObd4dGNp--USG5or')
-let a7 = new Aluno("Ellen", "2 ano", "0292827AL", 16, 'https://drive.google.com/file/d/1xkGio1rSh25XTFnKAHcwXXsFliwU22HQ')
-let a8 = new Aluno("Thayslane", "2 ano", "36436AL", 17, 'https://drive.google.com/file/d/1kQcfoPxzC3k0Jje8t6Qwo8we7JQhsdvR')
-let a9 = new Aluno("Catarina", "2 ano", "376253AL", 16, 'https://drive.google.com/file/d/1tCJ1hVCp7445_lj-Dj4LZ0yOaTAfxwA2')
-let a10 = new Aluno("Rebeca", "2 ano", "297373AL", 16, 'https://drive.google.com/file/d/1vw8sfjAXTlZpfwTJNJSXJQd1sOgKiVb0')
-let a11 = new Aluno("Helysson", "2 ano", "3736552AL", 17, 'https://drive.google.com/file/d/1IVyT8vEVgMC7UoKOcgqo3Nol9tBSmSLs')
+let a2 = new Aluno("Darline", 'Silva', "2 ano", "8947152AL", '29/10/2006', 'https://drive.google.com/file/d/1mGY2Y1oyIks5f7OvgCtd5lEysC5oe1uX')
+let a3 = new Aluno("Jaciane", 'Santos', "2 ano", "3326453AL", '04/03/2007', 'https://drive.google.com/file/d/1d5oA9rmApqBH6Mt3n75Dto7bETmuus6x')
+let a4 = new Aluno("Girleide", 'Silva', "2 ano", "098635AL", '03/09/2006', 'https://drive.google.com/file/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r')
+let a5 = new Aluno("Thiago",  'Melo',"2 ano", "92863AL", '12/02/2007', 'https://drive.google.com/file/d/1yK1BWGSRxuHt4gP6mmYNOIRHs4plJ8gR')
+let a6 = new Aluno("Kemylly", 'Rodrigues', "2 ano", "28364AL", '04/03/2006', 'https://drive.google.com/file/d/1uWL4Z7JRW478UM_pObd4dGNp--USG5or')
+let a7 = new Aluno("Ellen", 'Viana', "2 ano", "0292827AL", '05/12/2006', 'https://drive.google.com/file/d/1xkGio1rSh25XTFnKAHcwXXsFliwU22HQ')
+let a8 = new Aluno("Thayslane", 'Nascimento', "2 ano", "36436AL", '10/07/2006', 'https://drive.google.com/file/d/1kQcfoPxzC3k0Jje8t6Qwo8we7JQhsdvR')
+let a9 = new Aluno("Catarina", 'Silva', "2 ano", "376253AL", '24/05/2007', 'https://drive.google.com/file/d/1tCJ1hVCp7445_lj-Dj4LZ0yOaTAfxwA2')
+let a10 = new Aluno("Rebeca", 'Santos', "2 ano", "297373AL", '07/09/2006', 'https://drive.google.com/file/d/1vw8sfjAXTlZpfwTJNJSXJQd1sOgKiVb0')
+let a11 = new Aluno("Helysson", 'Silva', "2 ano", "3736552AL", '11/06/2006', 'https://drive.google.com/file/d/1IVyT8vEVgMC7UoKOcgqo3Nol9tBSmSLs')
 
 
 let listaDeAlunos = [a2, a3, a4, a5, a6, a7, a8, a9 ,a10, a11]
 
 listaDeAlunos.sort((a, b) => {
-    if (a.nome[0] > b.nome[0]) {
+    if (a.primeiroNome[0] > b.primeiroNome[0]) {
         return 1
-    } else if (a.nome[0] < b.nome[0]){
+    } else if (a.primeiroNome[0] < b.primeiroNome[0]){
         return -1
     } else {
         return 0
@@ -29,7 +29,7 @@ for(let i = 0; i < listaDeAlunos.length; i++){
         <div class='cabecalho'>
             <div>
                 <p class='turma'>${listaDeAlunos[i].turma}</p>
-                <h3 class='nome'>${listaDeAlunos[i].nome}</h3>
+                <h3 class='nome'>${listaDeAlunos[i].getNome()}</h3>
             </div>
             <img class='logo' src='img/logoifal.png'/>
         </div>
@@ -39,7 +39,7 @@ for(let i = 0; i < listaDeAlunos.length; i++){
             <div class='info'>
                 <div>
                     <p class='label'>Idade</p>
-                    <p class='valor'>${listaDeAlunos[i].idade}</p>
+                    <p class='valor'>${listaDeAlunos[i].getIdade()}</p>
                 </div>
                 <div>
                     <p class='label'>Matrícula</p>
