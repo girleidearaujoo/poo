d2ef12953020511a53b966a6df9654f924d799b1
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-09-18 09:02:08 -0300

    Update designer.js

--- [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/designer.js
+++ [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/designer.js
@@ -1,28 +1,30 @@
-for(let i = 0; i < listaDeAlunos.length; i++){
-    document.write(`
-    <div class='card'>
-
-        <div class='cabecalho'>
-            <div>
-                <p class='turma'>${listaDeAlunos[i].turma}</p>
-                <h3 class='nome'>${listaDeAlunos[i].getNome()}</h3>
-            </div>
-            <img class='logo' src='img/logoifal.png'/>
-        </div>
+function desenharCards(container) {
+    for(let i = 0; i < listaDeAlunos.length; i++){
+     CredentialsContainer.innerHTML +=   `
+        <div class='card'>
     
-        <div class='conteudo'>
-            <img class='foto' src='${listaDeAlunos[i].foto.replace('https://drive.google.com/file', 'https://lh3.googleusercontent.com')}'>
-            <div class='info'>
+            <div class='cabecalho'>
                 <div>
-                    <p class='label'>Idade</p>
-                    <p class='valor'>${listaDeAlunos[i].getIdade()}</p>
+                    <p class='turma'>${listaDeAlunos[i].turma}</p>
+                    <h3 class='nome'>${listaDeAlunos[i].getNome()}</h3>
                 </div>
-                <div>
-                    <p class='label'>Matrícula</p>
-                    <p class='valor'>${listaDeAlunos[i].matricula}</p>
+                <img class='logo' src='img/logoifal.png'/>
+            </div>
+        
+            <div class='conteudo'>
+                <img class='foto' src='${listaDeAlunos[i].foto.replace('https://drive.google.com/file', 'https://lh3.googleusercontent.com')}'>
+                <div class='info'>
+                    <div>
+                        <p class='label'>Idade</p>
+                        <p class='valor'>${listaDeAlunos[i].getIdade()}</p>
+                    </div>
+                    <div>
+                        <p class='label'>Matrícula</p>
+                        <p class='valor'>${listaDeAlunos[i].matricula}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-
-    </div>`)
+    
+        </div>`
+    }    
 }
