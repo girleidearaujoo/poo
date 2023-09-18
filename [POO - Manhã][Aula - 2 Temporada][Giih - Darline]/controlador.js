5b54c06a5855e8fff2546b017ceb8fb5d24060be
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-09-18 09:01:58 -0300

    Update controlador.js

--- [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/controlador.js
+++ [POO - Manhã][Aula - 2 Temporada][Giih - Darline]/controlador.js
@@ -1,1 +1,23 @@
-let inputTurma 
+let inputTurma = document.getElementById('iptTurma');
+let inputPrimeiroNome = document.getElementById('iptPrimeiroNome');
+let inputUltimoNome = document.getElementById('iptUltimoNome');
+let inputDataDeNascimento = document.getElementById('iptDataDeNascimento');
+let inputMatricula = document.getElementById('iptMatricula'); 
+let buttonCadastrar = document.getElementById('btnCadastrar'); 
+
+let containerDeCards = document.getElementById('containerCards')
+
+buttonCadastrar.addEventListener('click', quandoClicarEmCadasrar)
+function quandoClicarEmCadasrar() {
+    let alunoTemp = new Aluno(
+        inputPrimeiroNome.value,
+        inputUltimoNome.value,
+        inputTurma.value,
+        inputMatricula.value,
+        inputDataDeNascimento.value
+
+    );
+     listaDeAlunos.push(alunoTemp)
+     desenharCards(containerDeCards)
+     console.log(listaDeAlunos);
+}
