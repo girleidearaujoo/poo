2ecc2e5caadb09d016079bde0484ce343ac47df2
MatheusVinicius-pazz <gas27@aluno.ifal.edu.br>
2023-11-13 10:42:48 -0300

    ...

---
+++ [POO - Manhã][Projeto Final][Gi, Ellen e Leticia]/classes.js
@@ -0,0 +1,61 @@
+class Empreendedor{
+    constructor(nome, telefone, email, descricao, endereco){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.descricao = descricao;
+        this.endereco = endereco;
+    }
+}
+
+class Cliente{
+    constructor(nome, cpf, telefone, email){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        }
+}
+
+class Veterinario extends Empreendedor{
+    constructor(nome,  telefone, email, descricao, endereco, crmv){
+        super(nome, telefone, email, descricao, endereco);
+        this.crmv = crmv;
+    }
+}
+
+class Baba extends Empreendedor{
+    constructor(nome,  telefone, email, descricao, endereco, valor, cpf){
+        super(nome, telefone, email, descricao, endereco);
+        this.valor = valor;
+        this.cpf = cpf;
+    }
+}
+
+class PetShop extends Empreendedor{
+    constructor(nome,  telefone, email, descricao, endereco, vagas, cnpj){
+        super(nome, telefone, email, descricao, endereco);
+        this.vagas = vagas;
+        this.cnpj = cnpj;
+    }
+}
+
+class Hotel extends Empreendedor{
+    constructor(nome,  telefone, email, descricao, endereco, vagas, cnpj, tiposDeQuarto, horario_inicio, horario_termino, avaliacao){
+        super(nome, telefone, email, descricao, endereco);
+        this.vagas = vagas;
+        this.cnpj = cnpj;
+        this.tiposDeQuarto = tiposDeQuarto;
+        this.horario_inicio = horario_inicio;
+        this.horario_termino = horario_termino;
+        this.avaliacao = avaliacao;
+    }
+}
+
+class Transacao{
+    constructor(){
+        
+    }
+}
+
+
